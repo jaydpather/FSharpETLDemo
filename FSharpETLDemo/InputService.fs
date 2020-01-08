64c0c644178dd3eac8d073166fc0f9e655941f83
Jayd Pather <jaydpather@pvh.com>
2020-01-08 09:20:22 +0100

    logging message for missing CustomerCompany record

--- FSharpETLDemo/InputService.fs
+++ FSharpETLDemo/InputService.fs
@@ -7,6 +7,13 @@ open System
 
 let loadCustomers loadCustomersRepoFunc =
     try
-        loadCustomersRepoFunc()
+        let loadedCustomer = loadCustomersRepoFunc()
+        match loadedCustomer with 
+        | Some (customer:SAPCustomer) ->
+            match customer.CompanyCode with 
+            | null //never seen null, but maybe this will change with later versions of SqlDataReader
+            | "" -> Failure (String.Format("CustomerNumber {0} has no CompanyCode. (missing CustomerCompany record)", customer.CustomerNumber))
+            | _ -> Success (Some customer)
+        | None -> Success None //Success None means there were no records to load
     with
         | :? Exception as ex -> String.Format("Exception when trying to load input customers.{2}Message:{0},{2}Stack Trace:{1}", ex.StackTrace, ex.Message, Environment.NewLine) |> Failure 
--- InputDataLayer/InputRepository.fs
+++ InputDataLayer/InputRepository.fs
@@ -26,21 +26,23 @@ let loadCustomers () =
 	  ,CC.CompanyCode
 	  ,C.[Id] As CustomerId  
 	FROM [dbo].[CustomerBasic] CB
-	INNER JOIN [dbo].[CustomerCompany] CC ON CC.CustomerNumber=CB.CustomerNumber
-	LEFT OUTER JOIN [WeConnectSales_Monkey].[dbo].[Customers] C ON (C.CustomerNumber=CB.CustomerNumber AND C.CompanyCode=CC.CompanyCode)
-	WHERE CC.CompanyCode IN ('W031','TH31','INLC','TH90','TH47','CK07','CK47','PB31','3906','PVHE')"
+	LEFT JOIN [dbo].[CustomerCompany] CC ON CC.CustomerNumber=CB.CustomerNumber
+	LEFT OUTER JOIN [WeConnectSales_Monkey].[dbo].[Customers] C ON (C.CustomerNumber=CB.CustomerNumber AND C.CompanyCode=CC.CompanyCode)"
+    //WHERE CC.CompanyCode IN ('W031','TH31','INLC','TH90','TH47','CK07','CK47','PB31','3906','PVHE')
     
     //todo: reusable method for command and connection
     use sqlConn = new SqlConnection("Server=AMSAPP012\Dev;Initial Catalog=SAPImport_Monkey;Trusted_Connection=true;") //todo: connection string in app.config
     sqlConn.Open()
     use sqlCmd = new SqlCommand(query, sqlConn) //todo: does F# have String.Empty?
     use dataReader = sqlCmd.ExecuteReader();
-    
-    //todo: actually don't need this sequence thing since we're only selecting 1 record
-    //todo: return 1 SAPCustomer OPTION instead of a SAPCustomer list. (you need option in case it loaded 0 records)
+
     //todo: what is the difference between Seq and List?
     //todo: does Seq.toList create a new list?
-    let customer = 
+    //NOTE: it seems that if a string is null in the DB, it comes out as "" in F#, so there is no need to check for null
+    //  * this also means if you want your F# code to distinguish between NULL and "", you need to use a SQL CASE statement
+    //todo: new function for matching dataReader.Read(), param: lambda that calls dataReader.Read()
+    //todo: new function for creating Some (customer), takes dataReader as param (so you can inject this)
+    let loadedCustomer = 
         match dataReader.Read() with
             | true -> Some {
                     CustomerNumber = (string)dataReader.["CustomerNumber"]; 
@@ -58,5 +60,4 @@ let loadCustomers () =
                     CountryCode = (string)dataReader.["CountryCode"];
                 }
             | false -> None
-            
-    Success (customer) 
+    loadedCustomer
